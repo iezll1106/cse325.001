02febd9a16afbac2c99c16d80ac4a2e563c9ff7e
Liezl Lizardo <llizardo@byupathway.edu>
2025-12-04 18:45:05 +0800

    Fix: Convert BlazingPizza from submodule to normal folder

---
+++ BlazingPizza/Model/PizzaSpecial.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;   // <-- REQUIRED
+
+namespace BlazingPizza
+{
+    /// <summary>
+    /// Represents a pre-configured template for a pizza a user can order
+    /// </summary>
+    public class PizzaSpecial
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string GetFormattedBasePrice()
+            => BasePrice.ToString("C", CultureInfo.GetCultureInfo("en-PH"));
+    }
+}
