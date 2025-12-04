18a76016eec6ac206473affa308869476606ddca
Liezl Lizardo <llizardo@byupathway.edu>
2025-12-04 17:50:38 +0800

    Removed duplicate Pizza.cs from Data folder

--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -1,29 +1,40 @@
-using System.Globalization;  // <-- Add this
+using System.Globalization;
+using BlazingPizza.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ----------------------------------------------------
+// REGISTER SERVICES
+// ----------------------------------------------------
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+// Register your custom services
+builder.Services.AddSingleton<PizzaService>();
+builder.Services.AddSingleton<OfficeService>();
+builder.Services.AddSingleton<UserService>();
+
+// Optional: builder.Services.AddLocalization();
+
 var app = builder.Build();
 
-//
-// >>> ADD THIS (Set global culture to en-PH) <<<
-//
+// ----------------------------------------------------
+// SET GLOBAL CULTURE (en-PH)
+// ----------------------------------------------------
 var cultureInfo = new CultureInfo("en-PH");
 CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
 CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
-//
 
-// Optional: For localization if you need later
-// builder.Services.AddLocalization();
-
+// ----------------------------------------------------
+// MIDDLEWARE
+// ----------------------------------------------------
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
 
 app.UseStaticFiles();
+
 app.UseRouting();
 
 app.MapRazorPages();
